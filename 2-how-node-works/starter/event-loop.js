7f6791be91b82037d25d3b7af9c42bad007ca27a
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2023-08-15 14:15:36 +0530

    event loop practiced in details :)

---
+++ 2-how-node-works/starter/event-loop.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 2;
+
+setTimeout(() => console.log("Timer 1 Finished"), 0);
+setImmediate(() => console.log("Immediate 1 Finished"));
+
+fs.readFile("./test-file.txt", "utf-8", (err, data) => {
+  console.log("IO Finished");
+  setTimeout(() => console.log("Timer 2 Finished"), 0);
+  setTimeout(() => console.log("Timer 3 Finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 Finished"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password Encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password Encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password Encrypted");
+  
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password Encrypted");
+});
+
+console.log("Hello from top level code");
